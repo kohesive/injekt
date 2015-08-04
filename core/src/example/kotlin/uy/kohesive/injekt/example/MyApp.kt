8e42155d6f030f364fb104a86cdfeca24a4ac027
jaysonminard <jayson.minard@gmail.com>
2015-08-04 19:41:50 -0300

    Starting an Example app

---
+++ core/src/example/kotlin/uy/kohesive/injekt/example/MyApp.kt
@@ -0,0 +1,72 @@
+package uy.kohesive.injekt.example
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import uy.kohesive.injekt.*
+import kotlin.platform.platformStatic
+import kotlin.properties.Delegates
+
+class MyApp {
+    companion object : InjektModule() {
+        // my app starts here with a static main()
+        platformStatic public fun main(args: Array<String>) {
+            MyApp().run()
+        }
+
+        // the InjektModule() will call me back here on a method I override.  And all my functions for registration are
+        // easy to find on the receiver class
+        override fun InjektRegistrar.registerInjektables() {
+            // let's setup my logger first
+            addLoggerFactory<Logger>({ byName -> LoggerFactory.getLogger(byName) }, { byClass -> LoggerFactory.getLogger(byClass) })
+
+            // now some singletons
+            addSingleton(HttpServerConfig("0.0.0.0", 8080, 16))
+            addSingleton(DatabaseConnectionConfig("db.myhost.com", "sa", "sillycat"))
+
+            // or a lazy singleton factory
+            addSingletonFactory { DontCreateUntilWeNeedYa() }
+
+            // or lets only have one database connection per thread, basically a singleton per thread
+            addPerThreadFactory { JdbcDatabaseConnection(Injekt.get()) }  // wow, nested injektions!!!
+
+            // or give me a new one each time it is injekted
+            addFactory { LazyDazy() }
+
+            // or be weird and use extension functions on classes that are visible while in this lambda
+            KnownObject().registerAsSingleton()
+            KnownObject::class.registerFactory { KnownObject() }
+
+            // and we also have factories that use a key (or single parameter) to return an instance
+            val pets = listOf(NamedPet("Bongo"), NamedPet("Dancer"), NamedPet("Cheetah")).map { it.name to it}.toMap()
+            addPerKeyFactory { petName: String -> pets.get(petName) }
+        }
+    }
+
+    // === Ok, now let's use these factories ===
+
+    // Injektions to properties can be delegates, we've added a bunch of extensions for inject now, lazy, and more
+    val log: Logger by Delegates.injektLogger()
+    val laziest: LazyDazy by Delegates.injektLazy()
+    val lessLazy: LazyDazy by Delegates.injektValue()
+
+    public fun run() {
+        // even local variables can be injekted, or rather "got"
+        val something = Injekt.get<DontCreateUntilWeNeedYa>()
+        startHttpServer()
+    }
+
+    // and we can inject into methods by using Kotlin default parameters
+    public fun startHttpServer(httpCfg: HttpServerConfig = Injekt.get()) {
+        log.debug("HTTP Server starting on ${httpCfg.host}:${httpCfg.port}")
+        HttpServer(httpCfg.host, httpCfg.port).withThreads(httpCfg.workerThreads).handleRequest { context ->
+            val db: JdbcDatabaseConnection = Injekt.get()    // we have a connection per thread now!
+
+            if (context.params.containsKey("pet")) {
+                // injekt from a factory that requires a key / parameter
+                val pet: NamedPet = Injekt.get(context.params.get("pet")!!)
+                // or other form without reified parameters
+                val pet2 = Injekt.get<NamedPet>(context.params.get("pet")!!)
+            }
+        }
+    }
+}
---
+++ core/src/example/kotlin/uy/kohesive/injekt/example/SillyClasses.kt
@@ -0,0 +1,19 @@
+package uy.kohesive.injekt.example
+
+// mock classes used for the example
+
+data class HttpServerConfig(val host: String, val port: Int, val workerThreads: Int)
+data class HttpContext(val params: Map<String, String> = emptyMap())
+data class HttpServer(val host: String, val port: Int) {
+    public fun withThreads(threadCount: Int): HttpServer  {}
+    public fun handleRequest(handle: (context: HttpContext)->Unit) { }
+}
+
+
+data class DontCreateUntilWeNeedYa()
+data class LazyDazy()
+data class NamedPet(val name: String)
+data class KnownObject()
+
+data class DatabaseConnectionConfig(val host: String, username: String, password: String)
+data class JdbcDatabaseConnection(val cfg: DatabaseConnectionConfig)
